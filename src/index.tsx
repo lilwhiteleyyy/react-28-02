5dee6d9404ef0af320c25bc66bae01f223afee4c
lilwhiteleyyy <lilwhiteleyyy@gmail.com>
2023-05-20 00:23:43 +0200

    add JSX

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,21 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 // React Element
-const title = React.createElement(`h1`, {id: `title`}, `Hello React` )
+const title = React.createElement(`h1`, { id: `title` }, `Hello React`)
+
+// JSX
+
+const app = (
+    <div>
+        <h1 id="text">Hello App</h1>
+        <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestias,
+            earum alias? Earum nihil esse aspernatur harum magni corporis, sequi
+            delectus minima ipsum rem debitis excepturi natus, sunt ex maxime
+            cum.
+        </p>
+    </div>
+)
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-root.render(<React.StrictMode>{title}</React.StrictMode>)
+root.render(<React.StrictMode>{app}</React.StrictMode>)
