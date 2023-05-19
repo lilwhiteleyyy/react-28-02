c580d569379c076e0e3b26317f875bd8c7fa1b2e
lilwhiteleyyy <lilwhiteleyyy@gmail.com>
2023-05-20 00:34:00 +0200

    add h1 and p

--- src/index.tsx
+++ src/index.tsx
@@ -6,17 +6,28 @@ const title = React.createElement(`h1`, { id: `title` }, `Hello React`)
 
 // JSX
 
-const app = (
-    <div>
-        <h1 id="text">Hello App</h1>
-        <p>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestias,
-            earum alias? Earum nihil esse aspernatur harum magni corporis, sequi
-            delectus minima ipsum rem debitis excepturi natus, sunt ex maxime
-            cum.
-        </p>
-    </div>
-)
+// const app = (
+//     <div>
+//         <h1 id="text">Hello App</h1>
+//         <p>
+//             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestias,
+//             earum alias? Earum nihil esse aspernatur harum magni corporis, sequi
+//             delectus minima ipsum rem debitis excepturi natus, sunt ex maxime
+//             cum.
+//         </p>
+//     </div>
+// )
+
+const App = () => {
+    return ( <div><h1>Hello App component</h1>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cupiditate ad, cumque, reprehenderit maxime fuga sit id non ipsum quidem pariatur ipsam quam voluptatem consectetur sed accusantium optio ea explicabo cum?</p>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cupiditate ad, cumque, reprehenderit maxime fuga sit id non ipsum quidem pariatur ipsam quam voluptatem consectetur sed accusantium optio ea explicabo cum?</p>
+    </div> 
+    )
+}
+
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-root.render(<React.StrictMode>{app}</React.StrictMode>)
+root.render(<React.StrictMode>
+    <App/>
+</React.StrictMode>)
